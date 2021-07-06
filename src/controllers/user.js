b2d631646b9a703f25ac31de25beb403fe07dd27
digitalhedu <evirgilio@digitalhouse.com>
2021-07-06 17:56:02 -0300

    Update Proyect

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,21 +1,21 @@
 const { validationResult } = require('express-validator');
 const userModel = require("../models/user");
 module.exports = {
-  login: (req,res) => res.render("/user/login",{title:"Access"}),
-  register: (req,res) => res.render("/user/register",{title:"Join"}),
+  login: (req,res) => res.render("users/login",{title:"Access"}),
+  register: (req,res) => res.render("users/register",{title:"Join"}),
   save: (req,res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.render("/user/register",{ errors: errors.mapped(),title:"Join" });
+      return res.render("users/register",{ errors: errors.mapped(),title:"Join" });
     }else{
       userModel.save(req.body);
-      return res.redirect("/user/login");
+      return res.redirect("users/login");
     }
   },
   access: (req,res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.render("/user/login",{ errors: errors.mapped(),title:"Access" });
+      return res.render("users/login",{ errors: errors.mapped(),title:"Access" });
     }else{
       let user = userModel.findByEmail(req.body.email);
       if(req.body.remember){
@@ -25,7 +25,7 @@ module.exports = {
       return res.redirect("/")
     }
   },
-  profile:(req,res) => res.render("/user/profile",{title:"Profile"}),
+  profile:(req,res) => res.render("users/profile",{title:"Profile"}),
   update: (req,res) => {
     userModel.update(req.body,null);
     delete req.session.user;
--- src/middlewares/styles.js
+++ src/middlewares/styles.js
@@ -1,6 +1,6 @@
 module.exports = (req,res,next) => {
   let path = String(req.url).split("/").pop();
-  res.locals.styles = ["https://meyerweb.com/eric/tools/css/reset/reset.css","https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css","https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"]
+  res.locals.styles = ["https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css","https://meyerweb.com/eric/tools/css/reset/reset.css","https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"]
   if(path > 0 ){
     res.locals.styles.push(`/css/${path}.css`)
   }else{
